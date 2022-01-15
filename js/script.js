809050a6507a2993b0a811378164850826e14a04
Alvaro <alvarolangeok@gmail.com>
2022-01-15 17:08:42 -0300

    primera configuración, todas las paginas con nav y footer, fotos subidas, normalize animate wowjs linkeado

---
+++ js/script.js
@@ -0,0 +1,70 @@
+
+
+
+/* Esta pidiendo un numero que sea menor o igual a -10 */
+/*
+let num = 1
+while (num >= -10) {
+    alert (num)
+    num -- // Incrementa de a 1
+}
+alert ("El número es mayor a 10") 
+*/
+
+
+
+
+
+
+
+// for
+/*
+for (let i = 0; i < 10; i++) {
+    alert ("i vale=" +i)
+}
+*/
+
+
+// Lo mismo seria
+/*
+let x=0
+for (x; x < 10; x++ )
+*/
+
+
+
+
+
+// Tabla de multiplicar
+/* let dato = parseInt (prompt ("Ingrese un número"))
+for (let num=1 ; num<=10 ; num++){
+    /*if (num==5){
+        break; //se corta despues del 5 y no sigue, se abre directamente la web
+    }*
+if (num==5){
+    continue; //continua despues del 5 hasta el numero que le pediste 
+}
+
+
+    let resultado = dato * num
+    alert (dato + "X" + num + "=" + resultado)
+} */
+
+
+/*
+let dia = prompt ("Ingrese un día").toLocaleLowerCase()
+    switch (dia) {
+        case "lunes":
+            alert ("Andá a laburar")
+            break;
+        case "martes":
+            alert ("Andá a laburar")
+            break;  
+        case "miercoles":
+            alert ("Andá a laburar")
+            break; 
+        default:
+            alert ("Hoy no laburas")
+            break; 
+    }
+*/
